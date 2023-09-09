636831a944f92a85060335091dd14fbb6aacdc28
Proloy Mishra <namitapro0987@gmail.com>
2023-09-09 19:23:30 +0530

    use hex for I/O instead of binary

--- src/io.rs
+++ src/io.rs
@@ -1,5 +1,6 @@
 use std::{
     io::{self, Read, Write},
+    num::ParseIntError,
     rc::Rc,
 };
 
@@ -11,20 +12,7 @@ pub(crate) fn input(e: &mut Expr) {
 }
 
 fn encode(byte: Option<u8>) -> Expr {
-    let src = byte.map_or("Eof()".into(), |b| {
-        let enc = |idx| ["F", "T"][(b >> idx) as usize & 1];
-        format!(
-            "{} {} {} {} {} {} {} {}()",
-            enc(7),
-            enc(6),
-            enc(5),
-            enc(4),
-            enc(3),
-            enc(2),
-            enc(1),
-            enc(0),
-        )
-    });
+    let src = byte.map_or("Eof()".into(), |b| format!("{:02X}()", b));
 
     crate::parse_expr(src).unwrap_or_else(|e| {
         e.report();
@@ -35,11 +23,7 @@ fn encode(byte: Option<u8>) -> Expr {
 pub(crate) fn output(e: &mut Expr) {
     if let Expr::Func(f) = e {
         let args = &mut f.args;
-        let b = if args.is_empty() {
-            0
-        } else {
-            decode(0, 8, &args[0])
-        };
+        let b = decode(args.get(0)).unwrap_or(0);
         io::stdout().write_all(&[b]).unwrap();
         if !args.is_empty() {
             *e = args[0].clone();
@@ -49,26 +33,9 @@ pub(crate) fn output(e: &mut Expr) {
     }
 }
 
-fn decode(res: u8, idx: u8, e: &Expr) -> u8 {
-    if idx == 0 {
-        return res;
-    }
+fn decode(e: Option<&Expr>) -> Result<u8, ParseIntError> {
     match e {
-        Expr::RedFunc(f) if f.name == "T" => {
-            let res = res | (1 << idx - 1);
-            if let Some(x) = f.args.get(0) {
-                decode(res, idx - 1, x)
-            } else {
-                res >> idx - 1
-            }
-        }
-        Expr::RedFunc(f) if f.name == "F" => {
-            if let Some(x) = f.args.get(0) {
-                decode(res, idx - 1, x)
-            } else {
-                res >> idx - 1
-            }
-        }
-        _ => res >> idx - 1,
+        Some(Expr::RedFunc(f)) => u8::from_str_radix(&f.name, 16),
+        _ => u8::from_str_radix("", 16),
     }
 }
